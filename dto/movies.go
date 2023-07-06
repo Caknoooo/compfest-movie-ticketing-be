0ba105d257e02be9e7e07763c6c1e93a07bcf069
Caknoooo <caknocomel@gmail.com>
2023-07-06 17:12:13 +0700

    feat: create seeder for movie

--- dto/movies.go
+++ dto/movies.go
@@ -4,6 +4,7 @@ type MovieCreateDTO struct {
 	Title       string `json:"title" form:"title" binding:"required"`
 	Description string `json:"description" form:"description" binding:"required"`
 	ReleaseDate string `json:"release_date" form:"release_date" binding:"required"`
+	PosterUrl	 string `json:"poster_url" form:"poster_url" binding:"required"`
 	AgeRating   int    `json:"age_rating" form:"age_rating" binding:"required"`
 	TicketPrice       float64    `json:"ticket_price" form:"price" binding:"required"`
 }
@@ -12,6 +13,7 @@ type MovieUpdateDTO struct {
 	Title       string `json:"title" form:"title"`
 	Description string `json:"description" form:"description"`
 	ReleaseDate string `json:"release_date" form:"release_date"`
+	PosterUrl	 string `json:"poster_url" form:"poster_url"`
 	AgeRating   int    `json:"age_rating" form:"age_rating"`
 	TicketPrice       float64    `json:"ticket_price" form:"price"`
 }
--- entities/movies.go
+++ entities/movies.go
@@ -11,6 +11,7 @@ type Movies struct {
 	Title string `gorm:"type:varchar(255)" json:"title"`
 	Description string `gorm:"type:text" json:"description"`
 	ReleaseDate time.Time `gorm:"timestamp with time zone" json:"release_date"`
+	PosterUrl string `gorm:"type:varchar(255)" json:"poster_url"`
 	AgeRating int `gorm:"type:int" json:"age_rating"`
 	TicketPrice float64 `gorm:"type:float" json:"ticket_price"`
 
